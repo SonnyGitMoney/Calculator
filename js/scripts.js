03fbfe27f381a53602b8af7cdf8c018581820ee6
Robert Onstott <roonstott@gmail.com>
2022-10-17 16:23:41 -0700

    Add further alert functions
    Co-authored-by Sonny Snyder <sendsonnymail@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,9 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
-window.alert(subtract(number1, number2));
-window.alert(multiply(number1, number2));
-window.alert(divide(number1, number2));
+window.alert("The result of adding " + number1 + " and " + number2 + " is " + add(number1, number2));
+window.alert("The result of subtracting " + number1 + " and " + number2 + " is " + subtract(number1, number2));
+window.alert("The result of multiplying " + number1 + " and " + number2 + " is " + multiply(number1, number2));
+window.alert("The result of dividing " + number1 + " and " + number2 + " is " + divide(number1, number2));
+
+window.alert("The result of adding " + number1 + " and " + number2 + " is " + add(number1, number2) + " The result of subtracting " + number1 + " and " + number2 + " is " + subtract(number1, number2) + " The result of multiplying " + number1 + " and " + number2 + " is " + multiply(number1, number2) + " The result of dividing " + number1 + " and " + number2 + " is " + divide(number1, number2));
